7754f37693b9b699ec544c41f94d2f81f7b0e876
minhchanh19130021 <93698433+minhchanh19130021@users.noreply.github.com>
2021-12-24 23:38:58 +0700

    full display

--- asset/js/filter.js
+++ asset/js/filter.js
@@ -1,72 +1,24 @@
 window.addEventListener("load", function() {
     var btnShows = document.querySelector(".js-show-filter");
     var btnClose = document.querySelector(".btn-exit");
+    var overlay = document.querySelector(".overlay");
     var modal = document.querySelector(".modal");
 
     function openModal() {
         modal.classList.add("openFilter");
+        overlay.classList.add("openFilter");
     }
 
     function closeModal() {
         modal.classList.remove("openFilter");
+        overlay.classList.remove("openFilter");
+    }
+
+    function close() {
+        overlay.classList.remove("openFilter");
     }
 
     btnShows.addEventListener("click", openModal);
     btnClose.addEventListener("click", closeModal);
-
-    // OPEN , CLOSE NAVIGATION
-    // const btnOpen = document.querySelector(".js-open-nav");
-    // const btnCloses = document.querySelector(".btn-moblie-tablet-exit");
-    // const nav = document.querySelector(".nav-mobile-tablet");
-
-    // function openNav() {
-    //     nav.classList.add("open");
-    // }
-
-    // function closeNav() {
-    //     nav.classList.remove("open");
-    // }
-
-    // btnOpen.addEventListener("click", openNav);
-    // btnCloses.addEventListener("click", closeNav);
-
-    // SCROLL
-
-    // OPEN MENU CHILD
-
-    //     const btnBrand = document.querySelector(".js-open-brand");
-    //     const btnAccessory = this.document.querySelector(".js-open-accessory");
-    //     const menuChilds1 = document.querySelectorAll(".menu-child-mobile-tablet1");
-    //     const menuChilds2 = document.querySelectorAll(".menu-child-mobile-tablet2");
-    //     const iconDown = document.querySelector(".js-open-accessory i");
-
-    //     var count = 0;
-    //     var countAccessory = 0;
-
-    //     function openMenuChildBrand() {
-    //         count++;
-    //         for (var menuChild of menuChilds1) {
-    //             if (count % 2 != 0) {
-    //                 menuChild.classList.add("openChild");
-    //                 iconDown.classList.add("rotate180");
-    //             } else {
-    //                 menuChild.classList.remove("openChild");
-    //                 iconDown.classList.remove("rotate180");
-    //             }
-    //         }
-    //     }
-
-    //     function openMenuChildAccessory() {
-    //         countAccessory++;
-    //         for (var menuChild of menuChilds2) {
-    //             if (countAccessory % 2 != 0) {
-    //                 menuChild.classList.add("openChild");
-    //             } else {
-    //                 menuChild.classList.remove("openChild");
-    //             }
-    //         }
-    //     }
-
-    //     btnBrand.addEventListener("click", openMenuChildBrand);
-    //     btnAccessory.addEventListener("click", openMenuChildAccessory);
+    overlay.addEventListener("click", close);
 });
