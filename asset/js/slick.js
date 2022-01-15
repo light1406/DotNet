9ceccdde93ef64b82362b7463097d5d25c708866
minhchanh19130021 <93698433+minhchanh19130021@users.noreply.github.com>
2022-01-15 17:14:50 +0700

    update register

--- asset/js/slick.js
+++ asset/js/slick.js
@@ -18,4 +18,24 @@ window.addEventListener("load", () => {
             },
         ],
     });
+    // $(".content").slick({
+    //     autoplay: true,
+    //     autoplaySpeed: 2000,
+    //     slidesToShow: 4,
+    //     prevArrow: '<div class="slick-prev"><i class="fas fa-arrow-left" aria-hidden="true"></i></div>',
+    //     nextArrow: '<div class="slick-next"><i class="fas fa-arrow-right" aria-hidden="true"></i></div>',
+    //     responsive: [{
+    //             breakpoint: 768,
+    //             settings: {
+    //                 arrows: false,
+    //             },
+    //         },
+    //         {
+    //             breakpoint: 480,
+    //             settings: {
+    //                 arrows: false,
+    //             },
+    //         },
+    //     ],
+    // });
 });
